435032ff174469529aae1504320298b5bddc29ce
Alex <alexcn711@gmail.com>
2022-03-29 08:31:03 -0600

    updates to dynamic bot

--- dynamic_chatbot/dynamic_chatbot.py
+++ dynamic_chatbot/dynamic_chatbot.py
@@ -1,23 +1,30 @@
 import openai
 import os
-import sys
 import interviewee
+import openai_communicator
 
 try:
     openai.api_key = os.environ["OPENAI_API_KEY"]
 except:
     print("OpenAI API key not set")
 
-num_args = len(sys.argv)
+template = "biographer"
+ai_interface = openai_communicator.OpenAICommunicator
 
 def run_interview():
     interview_subject = interviewee.Interviewee()
     interview_subject.summarize_person()
-    run_background_exchange()
+    run_background_exchange(interview_subject)
 
 # Casual conversation about their background info, dynamic back-and-forth
-def run_background_exchange():
-    pass
+def run_background_exchange(interview_subject):
+    print("OK, let's get started!")
+    prompt_question = ("I'm a " + template  + ", and I'm interviewing " + interview_subject.name + " today. " + interview_subject.name + " is"
+        + interview_subject.age + " years old and from " + interview_subject.where_from + " originally. They now live in " + interview_subject.residence
+        + " and their occupation is " + interview_subject.occupation + ". I thought " + interview_subject.name + " was a really interesting person, and "
+        + "I wanted to know more about them and the information they gave me. Here is a question I would ask " + interview_subject.name + " next: ")
+    response = ai_interface.call_openai(prompt_question)
+    print(response)
 
 # Begin
 run_interview()
--- dynamic_chatbot/openai_communicator.py
+++ dynamic_chatbot/openai_communicator.py
@@ -4,5 +4,5 @@ import openai
 class OpenAICommunicator:
     def call_openai(prompt, stop=None):
         # Run OpenAI with the newest DaVinci engine.
-        response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=64, stop=stop).choices[0].text
+        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=64, stop=stop).choices[0].text
         return response
